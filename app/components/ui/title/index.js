faef580190764c7f1a6a0296afceae9decbe84c2
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-10-08 14:41:31 -0300

    Title StyledComponent

--- app/components/ui/title/index.js
+++ app/components/ui/title/index.js
@@ -1,6 +1,18 @@
 import styled from 'styled-components'
 
-export const Title = styled.h1`
-  font-size: 28px;
-  font-weight: 500;
+export const Title = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  h1 {
+    display: flex;
+    align-items: center;
+    font-size: 24px;
+    font-weight: 500;
+  }
+
+  svg {
+    margin-right: 10px;
+  }
 `
