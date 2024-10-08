585cfc9fb33f9d913255b7ce16870874b9085b55
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-10-08 14:40:50 -0300

    Ajuste de layout

--- app/page.js
+++ app/page.js
@@ -1,28 +1,28 @@
 'use client'
 
-import { Title } from './components/ui/title'
 import { FaGithub, FaPlus } from 'react-icons/fa'
 import { Container } from './components/ui/container'
+import { Title } from './components/ui/title'
 import { Form } from './components/ui/form'
 import { SubmitButton } from './components/ui/submit-button'
 
 export default function Home() {
   return (
     <Container>
-      <main>
-        <Title>
+      <Title>
+        <h1>
           <FaGithub size={25} />
           Meus Repositórios
-        </Title>
+        </h1>
+      </Title>
 
-        <Form>
-          <input type="text" />
-          <SubmitButton>
-            <FaPlus size={16} />
-            <span className="sr-only">Adicionar repositório</span>
-          </SubmitButton>
-        </Form>
-      </main>
+      <Form>
+        <input type="text" />
+        <SubmitButton>
+          <FaPlus size={16} color="#f4f4f9" />
+          <span className="sr-only">Adicionar repositório</span>
+        </SubmitButton>
+      </Form>
     </Container>
   )
 }
