a1927ee76143ed49830cc83f3e73dd497a402c71
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-10-08 14:41:15 -0300

    Form StyledComponent

--- app/components/ui/form/index.js
+++ app/components/ui/form/index.js
@@ -1,3 +1,15 @@
-export function Form({ children }) {
-  return <>{children}</>
-}
+import styled from 'styled-components'
+
+export const Form = styled.form`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
+
+  input {
+    flex: 1;
+    border: 1px solid #ddd;
+    padding: 10px 15px;
+    border-radius: 4px;
+    font-size: 14px;
+  }
+`
