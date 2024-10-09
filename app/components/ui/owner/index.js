81ec793546bc013f8721eff43c57f0a491efb079
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-10-09 16:26:26 -0300

    Owner StyledComponent

---
+++ app/components/ui/owner/index.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export const Owner = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  img {
+    width: 90px;
+    border-radius: 20%;
+    margin: 20px 0;
+  }
+
+  h1 {
+    font-size: 18px;
+  }
+
+  p {
+    margin-top: 10px;
+    font-size: 14px;
+    text-align: center;
+    line-height: 1.4;
+    max-width: 400px;
+  }
+`
