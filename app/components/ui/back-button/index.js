b803a33cd2f92be9880ff9b5ecf716026e339221
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-10-09 16:23:21 -0300

    BackButton StyledComponents

---
+++ app/components/ui/back-button/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+import Link from 'next/link'
+
+export const BackButton = styled(Link)`
+  color: #1f2937;
+
+  :hover {
+    color: #f59e0b;
+    transition: color 0.2s ease;
+  }
+`
--- app/repository/[slug]/page.js
+++ app/repository/[slug]/page.js
@@ -1,9 +1,11 @@
 'use client'
 
 import { useState, useEffect } from 'react'
+import { FaArrowLeft } from 'react-icons/fa'
 import { Container } from '../../components/ui/container'
 import { Owner } from '../../components/ui/owner'
 import { Loading } from '../../components/ui/loading'
+import { BackButton } from '../../components/ui/back-button'
 import api from '../../../src/services/api'
 
 /* <p>My Repository: {params.slug}</p> */
@@ -16,22 +18,26 @@ export default function Page({ params }) {
     async function load() {
       const name = decodeURIComponent(params.slug) /** URL params  */
 
-      /** Fetch API's */
-      const [repository, issues] = await Promise.all([
-        api.get(`repos/${name}`),
-        api.get(`repos/${name}/issues`, {
-          params: {
-            state: 'open',
-            per_page: 5,
-          },
-        }),
-      ])
+      try {
+        /** Fetch API's */
+        const [repository, issues] = await Promise.all([
+          api.get(`repos/${name}`),
+          api.get(`repos/${name}/issues`, {
+            params: {
+              state: 'open',
+              per_page: 5,
+            },
+          }),
+        ])
 
-      setRepo(repository.data)
-      setOpenIssues(issues.data)
-      setLoading(false)
-
-      console.log(repository.data)
+        setRepo(repository.data)
+        setOpenIssues(issues.data)
+        setLoading(false)
+        console.log(repository.status)
+      } catch (error) {
+        console.log(error.message)
+        setLoading(false)
+      }
     }
 
     load()
@@ -47,7 +53,17 @@ export default function Page({ params }) {
 
   return (
     <Container>
+      <BackButton href="/">
+        <FaArrowLeft size={24} />
+        <span className="sr-only">Voltar</span>
+      </BackButton>
       <Owner>
+        {/* <img
+          src="https://avatars.githubusercontent.com/u/69631?s=200&v=4"
+          alt="facebook"
+        />
+        <h1>Facebok</h1>
+        <p>The library for web and native user interfaces.</p> */}
         <img src={repo.owner.avatar_url} alt={repo.owner.login} />
         <h1>{repo.name}</h1>
         <p>{repo.description}</p>
