3b05e0b573031e60b54354e4c6fdf87a3cb7d0d8
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-10-10 07:38:05 -0300

    IssuesList StyledComponent

--- app/components/ui/issues-list/index.js
+++ app/components/ui/issues-list/index.js
@@ -22,16 +22,38 @@ export const IssuesList = styled.ul`
     }
 
     div {
-      display: flex;
-      margin-left: 10px;
-    }
+      flex: 1;
+      margin-left: 12px;
 
-    strong {
-      font-size: 14px;
+      strong {
+        font-size: 14px;
 
-      a {
-        text-decoration: none;
-        color: #0089cc;
+        a {
+          text-decoration: none;
+          color: #0089cc;
+
+          &:hover {
+            color: #f59e0b;
+            transition: color 0.2s ease;
+          }
+        }
+
+        span {
+          background: #1f2937;
+          color: #f4f4f9;
+          border-radius: 4px;
+          font-size: 12px;
+          font-weight: 400;
+          padding: 3px 6px;
+          margin-left: 6px;
+        }
+      }
+
+      p {
+        margin-top: 10px;
+        font-size: 12px;
+
+        font-weight: 500;
       }
     }
   }
